5d1e82630a8bb4594ff5b1beb4b5f32685a10f60
Omar Amer <omahar1907@gmail.com>
2026-02-04 21:02:26 +0200

    Add Retrofit, RxJava, Glide dependencies for API integration

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -51,6 +51,32 @@ dependencies {
     implementation(libs.credentials.play.services.auth)
     implementation(libs.googleid)
     
+    // Retrofit for API calls
+    implementation("com.squareup.retrofit2:retrofit:2.9.0")
+    implementation("com.squareup.retrofit2:converter-gson:2.9.0")
+    implementation("com.squareup.retrofit2:adapter-rxjava3:2.9.0")
+    
+    // OkHttp for logging
+    implementation("com.squareup.okhttp3:logging-interceptor:4.12.0")
+    
+    // RxJava 3 (Required by project)
+    implementation("io.reactivex.rxjava3:rxjava:3.1.8")
+    implementation("io.reactivex.rxjava3:rxandroid:3.0.2")
+    
+    // Gson for JSON parsing
+    implementation("com.google.code.gson:gson:2.10.1")
+    
+    // Glide for image loading
+    implementation("com.github.bumptech.glide:glide:4.16.0")
+    annotationProcessor("com.github.bumptech.glide:compiler:4.16.0")
+    
+    // CardView and RecyclerView
+    implementation("androidx.cardview:cardview:1.0.0")
+    implementation("androidx.recyclerview:recyclerview:1.3.2")
+    
+    // SwipeRefreshLayout
+    implementation("androidx.swiperefreshlayout:swiperefreshlayout:1.1.0")
+    
     testImplementation(libs.junit)
     androidTestImplementation(libs.ext.junit)
     androidTestImplementation(libs.espresso.core)
